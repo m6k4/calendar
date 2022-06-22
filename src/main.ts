c4a3604336a773c4fa42d008e1087f27f0c417dc
m6k4 <olszewskazuzanna95@gmail.com>
2022-06-22 16:51:56 +0200

    (feature): add element ui and select

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import router from "./router";
 import 'font-awesome/css/font-awesome.css';
 import './index.css';
 import directives from "./directives/";
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
 const app = createApp(App);
 directives(app);
 app.use(router);
+app.use(ElementPlus);
 app.mount('#app');
 
--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -1,9 +1,22 @@
 <template>
-<select>
+  <el-select
+    class="p-8"
+    v-model="selectedView" 
+    placeholder="Select view"
+  >
+    <el-option
+      v-for="(view, index) in viewsTypes"
+      :key="index"
+      :label="view"
+      :value="view"
+    />
+  </el-select>
   <CalendarMonth 
+    v-if="selectedView === 'month'"
     :events-list="eventsList"
   />
-  <CalendarWeek
+  <CalendarWeek 
+    v-if="selectedView === 'week'"
     :events-list="eventsList"
   />
 </template>
@@ -11,9 +24,13 @@
 import CalendarMonth from '../components/Calendar/CalendarMonth/CalendarMonth.vue';
 import CalendarWeek from '../components/Calendar/CalendarWeek/CalendarWeek.vue';
 import useEvents from '../components/Calendar/composable/useEvents';
-
+import { ElSelect, ElOption } from 'element-plus'
+import { ref } from 'vue';
 const {
   eventsList,
 } = useEvents();
 
+const selectedView = ref('month');
+
+const viewsTypes = ['month', 'week']
 </script>
